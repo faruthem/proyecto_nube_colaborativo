fa61845a611f426ecf3f6b6c8aba2fc0dae5deaf
El_huron_del_verano(KLG) <k.lopezguzman@ugto.mx>
2022-11-14 20:01:32 -0600

    users
    Se modificó cmabiando send por render y agregando mi ruta, de esta forma el servidor puede localizar mis archivos y pintarlos

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,1 +1,15 @@
 //Usuario puede acceder a las URL para autenticarse login,logout, register,
+
+const express = require('express');
+const router = express.Router(); // ejecuta método  Router= creación de rutas
+
+router.get('/users/signin', (req, res)=>{
+    res.render('users/signin.hbs') //Con render lo envio ya de mi ruta y con send lo muestro, es un simple texto
+});
+
+router.get('/users/signup', (req, res) =>{
+    //res.send('form of authentication of ferrets');
+    res.render('users/signup.hbs')
+});
+
+module.exports = router;
