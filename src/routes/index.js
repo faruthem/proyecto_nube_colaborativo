c663e6d1ef35662fab4f71bc25b07d7cd477034e
El_huron_del_verano(KLG) <k.lopezguzman@ugto.mx>
2022-11-14 19:58:40 -0600

    index.js
    Se modificó la carpeta para que el server la pueda localizar, se agregó la función render para mostrar mi archivo en formato html y se realizó la configuración pertinente para pintar las notas en nuestro navegador

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,14 @@
 //Aquí van las URL de mi página principal, ejemplo /about
+const express = require('express');
+const router = express.Router(); // ejecuta método  Router= creación de rutas
+
+router.get('/', (req, res) => {
+    //res.send('pero si es el Index de la muerte que no se puede subir a Heroku');
+    res.render('index.hbs');
+});
+
+router.get('/about', (req, res) => {
+    //res.send('baia baia es mi About poderoso');
+    res.render('about.hbs');
+});
+module.exports = router;
