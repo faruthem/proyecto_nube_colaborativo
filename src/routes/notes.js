2ea694f8b6f5b3b6361524cc6b31cdb0b2a2b39b
El_huron_del_verano(KLG) <k.lopezguzman@ugto.mx>
2022-11-14 20:00:23 -0600

    notes
    modificación de archivo de notas, en un futuro se cambiará nombre por el apartado final que representará

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,1 +1,11 @@
 //Aqui vienene las rutas de mi servidor para que el usuaio pueda crear o manejar sus notas (crear, eliminar, actualizar)
+
+const express = require('express');
+const router = express.Router(); // ejecuta método  Router= creación de rutas
+
+router.get('/notes', (req, res) =>{
+    res.send('Notitas bonitas de la base de datos de los hurones más hermosos todavía')
+    //res.render('notas')//aquí va a buscar mis notas
+});
+
+module.exports = router;
