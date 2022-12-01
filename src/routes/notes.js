092e06fae1ee567ff87a3b26154da807d288614c
El_huron_del_verano(KLG) <k.lopezguzman@ugto.mx>
2022-11-30 18:54:57 -0600

    notes.js
    Se creó un formulario a manera de buzón de quejas y sugerencias para que el usuario pueda interactuar con el mismo, se creó de la misma forma, dos alertas para que no se permita dejar ninguno de los campos vacíos.

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -2,6 +2,43 @@
 
 const express = require('express');
 const router = express.Router(); // ejecuta método  Router= creación de rutas
+//requiero mis esquemas para almacenarlos
+const Note = require('../models/Note');
+
+
+//Aquí comienzo a crear mi formulario
+router.get('/notes/add', (req,res)=>{
+    res.render('notes/new-note');
+})
+//Aquí termino a crear mi formulario
+
+//ruta para enviar datos
+router.post('/notes/new-note', async (req,res) => {
+    const {title, description}=req.body;
+    const errors = [];
+    if(!title) {
+        errors.push({text: 'Por favor escribe un titulo para tu sugerencia'});
+    }
+    if(!description) {
+        errors.push({text: 'Por favor escribe una descripción para tu sugerencia'});
+    }
+    if (errors.length>0) {
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        });
+    }else{
+        //res.send('Sugerencia enviada');
+        const newNote = new Note({title, description});
+        //console.log(newNote);
+        await newNote.save();
+        //res.send('Sugerencia enviada y recibida');
+        res.redirect('/notes')
+    }
+    
+});
+//ruta para enviar datos
 
 router.get('/notes', (req, res) =>{
     res.send('Notitas bonitas de la base de datos de los hurones más hermosos todavía')
