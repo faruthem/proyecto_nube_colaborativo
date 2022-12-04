8c4af263c6f95d7e1667b2cb3a063ff119bbc8e6
El_huron_del_verano(KLG) <k.lopezguzman@ugto.mx>
2022-12-03 22:21:13 -0600

    Users
    Se agregó el modelo Users, nos permite agregar usuarios en el singup y pronto en el sigin

---
+++ src/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+//Importo modulo bcryp
+const bcrypt= require('bcryptjs');
+
+//Aquí guardamos la información que nos pasa signup
+const UserSchema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    date: {type: Date, default: Date.now}
+});
+
+//Aquí encriptamos nuestra contraseña
+UserSchema.methods.encryptPassword = async (password) =>{// llamo mi esquema.methods.nombre del metodo = contraseña
+  const salt = await bcrypt.genSalt(10);//la ciframos mediante bcryptjs ¿cómo? pues de forma bien asyncrona, replico 10 veces.
+  const hash = bcrypt.hash(password,salt);//Genero contraseña cifrada
+  return bcrypt.hash;
+};
+
+//Comparamos la contraseña encriptada, la que está guardada con la que está en la base de datos, chupale pichón
+UserSchema.methods.matchPassword = async function(password){//esquema.methods.nombre del metodo = contraseña
+  return await bcrypt.compare(password, this.password);// no utilizo función flecha para poder acceder a las propiedades fuera de la función
+};
+module.exports = mongoose.model('User',UserSchema);//Nombre del esquema en servidor y nombre de mi esquema en node.js 
+
