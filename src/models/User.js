bb2c4de6e9bbbdfdacc53853ce78ee3598cb6791
El_huron_del_verano(KLG) <k.lopezguzman@ugto.mx>
2022-12-05 12:43:12 -0600

    User.js
    Se  simplificó la forma en la que se genera la contraseña cifrada

--- src/models/User.js
+++ src/models/User.js
@@ -14,12 +14,11 @@ const UserSchema = new Schema({
 //Aquí encriptamos nuestra contraseña
 UserSchema.methods.encryptPassword = async (password) =>{// llamo mi esquema.methods.nombre del metodo = contraseña
   const salt = await bcrypt.genSalt(10);//la ciframos mediante bcryptjs ¿cómo? pues de forma bien asyncrona, replico 10 veces.
-  const hash = bcrypt.hash(password,salt);//Genero contraseña cifrada
-  return bcrypt.hash;
+  return await bcrypt.hash(password,salt);//Genero contraseña cifrada
 };
 
 //Comparamos la contraseña encriptada, la que está guardada con la que está en la base de datos, chupale pichón
-UserSchema.methods.matchPassword = async function(password){//esquema.methods.nombre del metodo = contraseña
+UserSchema.methods.matchPassword = async function(password) {//esquema.methods.nombre del metodo = contraseña
   return await bcrypt.compare(password, this.password);// no utilizo función flecha para poder acceder a las propiedades fuera de la función
 };
 module.exports = mongoose.model('User',UserSchema);//Nombre del esquema en servidor y nombre de mi esquema en node.js 
