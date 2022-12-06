e0c48d6dc4f5aba86688fa3b12014c7b75b0a61f
El_huron_del_verano(KLG) <k.lopezguzman@ugto.mx>
2022-12-06 09:58:19 -0600

    index.js
    Se agregó res.local.user = req.user || null lo que permite darle la bienvenidad al usuario con su nombre de login

--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ app.use((req,res,next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');//Para que me muestre los errores del login
+    res.locals.user = req.user || null;//Para darle la bienvenida al usuario
 
     next();
 });
