b70b3dda51dbb51b7192b5b701d840b7c13b20b7
El_huron_del_verano(KLG) <k.lopezguzman@ugto.mx>
2022-12-06 09:48:01 -0600

    Note.js
    Se agregó user: {type: String} lo que permite que nuestro esquema de notas tome el ID de la persona y lo case con este usuario, para que solo pueda ver sus notas

--- src/models/Note.js
+++ src/models/Note.js
@@ -1,3 +1,4 @@
+const Handlebars = require('handlebars')
 const mongoose = require ('mongoose');
 const { Schema } = mongoose;
 //Aquí le estoy diciendo como van a lucier mis datos
@@ -5,7 +6,8 @@ const { Schema } = mongoose;
 const NoteSchema = new Schema ({
     title: {type: String, requiered: true},
     description: { type: String, requiered: true},
-    date: {type: Date, default: Date.now}
+    date: {type: Date, default: Date.now},
+    user: {type: String}//Esto es para que cada nota se enlace a su usuario
 });
 
 //Con esto utilizo mi modelo en otras partes de mi aplicación
