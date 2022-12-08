ecfc52ffa97523ac0e77d62cf1b37ea9abb742c9
El_huron_del_verano(KLG) <k.lopezguzman@ugto.mx>
2022-12-08 10:31:35 -0600

    index.js
    Se agregaron nuevas rutas (artesanal, tienda, mexicana y datos).

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,8 +13,27 @@ router.get('/about', (req, res) => {
 });
 
 router.get('/news', (req, res) => {
-    //res.send('baia baia es mi About poderoso');
+    //res.send('baia baia es mi news poderoso');
     res.render('news.hbs');
 });
 
+router.get('/artesanal', (req, res) => {
+    //res.send('baia baia es mi artesanal poderoso');
+    res.render('artesanal.hbs');
+});
+
+router.get('/tienda', (req, res) => {
+    //res.send('baia baia es mi artesanal poderoso');
+    res.render('tienda.hbs');
+});
+
+router.get('/mexicana', (req, res) => {
+    //res.send('baia baia es mi artesanal poderoso');
+    res.render('mexicana.hbs');
+});
+
+router.get('/datos', (req, res) => {
+    //res.send('baia baia es mi artesanal poderoso');
+    res.render('datos.hbs');
+});
 module.exports = router;
