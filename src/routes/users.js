cd015b422cb56f2a5abe8d597c12026c7c1a9b81
El_huron_del_verano(KLG) <k.lopezguzman@ugto.mx>
2022-12-08 10:33:50 -0600

    users.js
    Se eliminó la propiedad lean () de las funciones, ya que generaba errores en el funcionamiento de la misma

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,6 +7,7 @@ const User = require('../models/User'); // importo mi esquema de usuarios YUJU!!
 const passport = require('passport'); // importamos para poder usar estrategy de autentication
 
 
+
 router.get('/users/signin', (req, res)=>{// esto sirve para que el usuario ingrese a la aplicación 
     res.render('users/signin'); //Con render lo envio ya de mi ruta y con send lo muestro, es un simple texto
 });
@@ -44,12 +45,12 @@ router.post('/users/signup', async (req, res) => {
     if (errors.length > 0) {
         res.render('users/signup', {errors, name, email, password, confirm_password}); //Si los errores son mayores a 0 entonces 
     }else{
-        const emailUser = await User.findOne({email: email}.lean()); //en caso de correo duplicado aquí estoy papá
+        const emailUser = await User.findOne({email: email}); //en caso de correo duplicado aquí estoy papá
         if (emailUser) {
             req.flash('error_msg','ADVERTENCIA, el correo ya está registrado');
             res.redirect('/users/signup');
         }
-        const newUser = new User({name, email, password}).lean();//Creo objeto llamado newUser <----quitar.lean() si algo sale mal
+        const newUser = new User({name, email, password});//Creo objeto llamado newUser <----quitar.lean() si algo sale mal
         newUser.password = await newUser.encryptPassword(password);//almaceno propiedad de objeto como password
         await newUser.save();//Aquí es donde ocurre la magia, aquí enviamos todos esos datos recolectados a nuestro esquema, lo que permite guardarlos en nuestra base de datos 
         req.flash('success_msg', 'Registro exitoso');
