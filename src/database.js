449a79c147dc967e68523a0ba06be85901710a2e
El_huron_del_verano(KLG) <k.lopezguzman@ugto.mx>
2022-12-14 00:29:55 -0600

    database
    Se agregó conexión a mi base de datos

--- src/database.js
+++ src/database.js
@@ -1,1 +1,13 @@
 //Conexión a una base de datos
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://kevinFerret:AWVW4H1BPBBFUFQL@clusterfinalnube.hyapnsf.mongodb.net/test', { //aquí estamos haciendo nuestra configuración con mongoose
+//mongoose.connect('mongodb://localhost:27017/CRUD', {
+         //const db = await connect("mongodb://localhost/crud");
+        //mongoose.connect("mongodb://localhost:27017/CRUD");
+
+    //useCreateIndex: true,
+    //useNewUrlParser: true,
+    //useFindAndModify: false,
+})
+    .then(db => console.log('BD creada por hurones conectada y armadita'))
+    .catch(err => console.error(err));
